aa61ed923f7e5565eb78f18d79995d3d37897947
Danny Barrientos <dannybarrientosagudelo@gmail.com>
2021-05-14 17:17:59 -0500

    Sanitizar caracteres con express validator

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { render } = require('pug');
 const router = express.Router();
 
+//TODO Importar Exxpress Validor 
+const { body } = require('express-validator/check')
+
 //TODO Importar el controllador
 const proyectoController = require
         ('../controller/proyectosController');
@@ -10,7 +13,9 @@ module.exports = function() {
         //ruta para el hombe
         router.get('/', proyectoController.proyectoHome)
         router.get('/nuevo-proyecto', proyectoController.formularioProyecto)
-        router.post('/nuevo-proyecto', proyectoController.nuevoProyecto)
+        router.post('/nuevo-proyecto', 
+        body('nombre').not().isEmpty().trim().escape(),
+        proyectoController.nuevoProyecto)
         return router;
 }
 
