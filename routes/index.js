a53c01b0fdeb48ea400b2c393db756e30010b17d
Danny Barrientos <dannybarrientosagudelo@gmail.com>
2021-05-14 13:55:45 -0500

    Ingresando al nuevo formulario

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ module.exports = function() {
         //ruta para el hombe
         router.get('/', proyectoController.proyectoHome)
         router.get('/nuevo-proyecto', proyectoController.formularioProyecto)
+        router.post('/nuevo-proyecto', proyectoController.nuevoProyecto)
         return router;
 }
 
