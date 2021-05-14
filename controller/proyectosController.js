d63b6916661312fda392cd44c3b6626ecb62732b
Danny Barrientos <dannybarrientosagudelo@gmail.com>
2021-05-14 15:49:38 -0500

    leyendo datos del formulario

--- controller/proyectosController.js
+++ controller/proyectosController.js
@@ -12,5 +12,22 @@ exports.formularioProyecto = (req, res) =>{
 
 exports.nuevoProyecto = (req, res) =>{
     //Enviar a la consola lo que envia el usuario
-    console.log(req.body);
+   //console.log(req.body);
+
+   //Validar que tengamos algo en el imput
+   const { nombre } = req.body;
+
+   let errores = [];
+   
+   if(!nombre) {
+       errores.push({'textoError':'Agrega un Nombre al Proyecto'})
+   }
+
+   //Si hay errores
+   if(errores.length>0) {
+       res.render('nuevoProyecto',{
+           nombrePagina: 'Nuevo Proyecto',
+           errores
+       })
+   }
 }
