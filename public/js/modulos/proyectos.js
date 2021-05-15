0ba3463b975dd7904c02bbf8e4c3a9af25279d63
Danny Barrientos <dannybarrientosagudelo@gmail.com>
2021-05-15 14:35:49 -0500

    Se puede tomar la url desde evento btn eliminar para enviar al BD

--- public/js/modulos/proyectos.js
+++ public/js/modulos/proyectos.js
@@ -3,28 +3,38 @@ import axios from 'axios';
 
 const btnEliminar = document.querySelector('#eliminar-proyecto');
 
-btnEliminar.addEventListener('click', () => {
-    // console.log('diste click en eliminar');
-    Swal.fire({
-        title: 'Deseas borrar este proyecto?',
-        text: "Un proyecto eliminado no se puede recuperar",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si, Borrar',
-        cancelButtonText: 'No, Cancelar'
-    }).then((result) =>{
-        if(result.value) {
-            Swal.fire(
-                'Proyecto Eliminado',
-                'El proyecto se ha eliminado ',
-                'success'
-            );
-            //redireccionar al incio, tambien lo puedo hacer con Node
-            setTimeout(() => {
-                window.location.href ='/'
-            }, 3000);
-        }
-    })
-});
+if(btnEliminar) {
+
+    btnEliminar.addEventListener('click', e=> {
+        const urlProyecto = e.target.dataset.proyectoUrl;
+
+        console.log(urlProyecto)
+        return;
+
+
+        Swal.fire({
+            title: 'Deseas borrar este proyecto?',
+            text: "Un proyecto eliminado no se puede recuperar",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Borrar',
+            cancelButtonText: 'No, Cancelar'
+        }).then((result) =>{
+            if(result.value) {
+                Swal.fire(
+                    'Proyecto Eliminado',
+                    'El proyecto se ha eliminado ',
+                    'success'
+                );
+                //redireccionar al incio, tambien lo puedo hacer con Node
+                setTimeout(() => {
+                    window.location.href ='/'
+                }, 3000);
+            }
+        })
+    });
+}
+ export default btnEliminar;
+
