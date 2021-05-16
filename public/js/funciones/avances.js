7617f6ddeaa464685079eafddc06649fedb981cd
Danny Barrientos <dannybarrientosagudelo@gmail.com>
2021-05-16 12:38:23 -0500

    Detectando el avance del proyecto

--- public/js/funciones/avances.js
+++ public/js/funciones/avances.js
@@ -1,19 +1,26 @@
+import Swal from 'sweetalert2';
+
 export const actualizarAvance = () => {
-    //TODO Seleccionar las tareas existente
-    const tareas = document.querySelector('li.tarea');
+    //TODO Seleccionar las tareas existentes
+    const tareas = document.querySelectorAll('li.tarea');
 
     if( tareas.length ) {
         //TODO Seleccionar las tareas completadas
         const tareasCompletas = document.querySelectorAll('i.completo');
 
         //TODO Calcular el avance
-        const avance = Math.round((tareasCompletas.length/tareas.length)*100)
+        const avance = Math.round((tareasCompletas.length / tareas.length) * 100);
 
-        //TODO Mostrar el avance
+       //TODO Mostrar el avance
         const porcentaje = document.querySelector('#porcentaje');
-        porcentaje.style.width = avance+'%'
+        porcentaje.style.width = avance+'%';
+
+        if(avance === 100) {
+            Swal.fire(
+                'Completaste el Proyecto',
+                'Felicidades, has terminado tus tareas',
+                'success'
+            )
+        }
     }
-
-
-
 }
